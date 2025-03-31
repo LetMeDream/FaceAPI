be87e03e09cba4ac9d1b6f24020769e6090b2dd9
Raul <raulalfonzo66@gmail.com>
2025-03-31 13:54:51 -0400

    feat (CameraContainer styling): add dinamyc styling to the CameraContainer component and use CSS variables to store the important repeated values

--- src/components/CameraContainer/CameraContainer.jsx
+++ src/components/CameraContainer/CameraContainer.jsx
@@ -5,7 +5,7 @@ import styled from 'styled-components';
 const StyledCircleFocus = styled.div.withConfig({
   shouldForwardProp: (prop) => prop !== 'isValid', // Filtra la prop isValid
 })`
-  height: 80%; /* 80% del padre de 480px; 384px */
+  height: var(--circle-focus-height); /* 80% del padre de 480px; 384px */
   aspect-ratio: 1 / 1;
   border: 1px solid ${({ isValid }) => (isValid ? 'green' : 'red')}; /* Cambia el color según isValid */
   position: absolute;
@@ -19,9 +19,29 @@ const CameraContainer = ({
   onPlay,
   isValid
 }) => {
+  const [maskImageRadius, setMaskImageRadius] = React.useState(null);
+
+  /* Automatically calculates --mask-image-radius */
+  React.useEffect(() => {
+    const calculateMaskImageRadius = () => {
+      const container = document.querySelector('.camera-container');
+      const styles = getComputedStyle(container);
+
+      const mainHeight = parseFloat(styles.getPropertyValue('--main-height')); // px
+      const circleFocusHeight = parseFloat(styles.getPropertyValue('--circle-focus-height')); // % 
+      const radius = (mainHeight * (circleFocusHeight/100))/2;
+      setMaskImageRadius(radius);
+    };
+
+    calculateMaskImageRadius();
+  }, []);
 
   return (
-        <div className='camera-container'>
+        <div className='camera-container'
+          style={{
+            '--mask-image-radius': maskImageRadius ? `${maskImageRadius}px` : undefined,
+          }}
+        >
           <video
             autoPlay
             playsInline
