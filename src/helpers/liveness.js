d6e895e5906addca826013cc3000879139ff2f93
Raul <raulalfonzo66@gmail.com>
2025-04-28 23:52:42 -0400

    feat: create helper functions required

--- src/helpers/liveness.js
+++ src/helpers/liveness.js
@@ -31,3 +31,105 @@ export const areRectanglesDifferent = (rect1, rect2) => {
     rect1.bottomRight.y !== rect2.bottomRight.y
   )
 }
+
+// Utility: Transform detection box coordinates.
+export const getInvertedFaceRectangle = (detection) => {
+      return {
+        topLeft: {
+          x: invertX(detection.box.bottomRight.x, detection.imageWidth),
+          y: detection.box.topLeft.y
+        },
+        topRight: {
+          x: invertX(detection.box.bottomLeft.x, detection.imageWidth),
+          y: detection.box.topRight.y
+        },
+        bottomLeft: {
+          x: invertX(detection.box.topRight.x, detection.imageWidth),
+          y: detection.box.bottomLeft.y
+        },
+        bottomRight: {
+          x: invertX(detection.box.topLeft.x, detection.imageWidth),
+          y: detection.box.bottomRight.y
+        }
+      };
+};
+
+// Utility: Get adjusted rectangle with margins.
+export const getSmallRectangle = (detection, margin) => {
+      return {
+        topLeft: {
+          x: detection.box.topLeft.x + 2 * margin,
+          y: detection.box.topLeft.y + margin
+        },
+        topRight: {
+          x: detection.box.topRight.x - 2 * margin,
+          y: detection.box.topRight.y + margin
+        },
+        bottomRight: {
+          x: detection.box.bottomRight.x - 2 * margin,
+          y: detection.box.bottomRight.y - margin
+        },
+        bottomLeft: {
+          x: detection.box.bottomLeft.x + 2 * margin,
+          y: detection.box.bottomLeft.y - margin
+        }
+      };
+};
+
+// Utility: Get real coordinates for the adjusted rectangle (inverted on x-axis)
+export const getSmallRectangleRealCoordinates = (detection, margin) => {
+      return {
+        topLeft: {
+          x: invertX(detection.box.bottomRight.x, detection.imageWidth, 2 * margin),
+          y: detection.box.topLeft.y + margin
+        },
+        topRight: {
+          x: invertX(detection.box.bottomLeft.x, detection.imageWidth, -2 * margin),
+          y: detection.box.topRight.y + margin
+        },
+        bottomRight: {
+          x: invertX(detection.box.topLeft.x, detection.imageWidth, -2 * margin),
+          y: detection.box.bottomRight.y - margin
+        },
+        bottomLeft: {
+          x: invertX(detection.box.topRight.x, detection.imageWidth, 2 * margin),
+          y: detection.box.bottomLeft.y - margin
+        }
+      };
+};
+
+/* Calculate Orientation */
+export const calculateOrientation = (detection) => {
+  const orientation = {
+    left: false,
+    right: false,
+    center: false
+  }
+  const importantLandmarks = [1, 15, 30];
+  const landmarks = detection.landmarks.positions;
+
+  const point1 = landmarks[1];   // Left edge of face
+  const point15 = landmarks[15]; // Right edge of face
+  const point30 = landmarks[30]; // Nose tip
+
+  const faceWidth = point15.x - point1.x;
+  const noseRelativeX = (point30.x - point1.x) / faceWidth;
+
+  if ((noseRelativeX < 0.4)) {
+    orientation.right = true;
+  } else if ((noseRelativeX > 0.6)) {
+    orientation.left = true;
+  } else {
+    orientation.center = true;
+  }
+
+  return orientation
+
+}
+
+/* LOCAL UTILITIES */
+
+// Utility: Invert an x coordinate given the image width.
+const invertX = (x, imageWidth, offset = 0) => {
+  return imageWidth - x + offset;
+};
