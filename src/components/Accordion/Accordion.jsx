782a1ff2f64c706c6ba38750df9fc0fda79e4920
Raul <raulalfonzo66@gmail.com>
2025-05-23 00:03:51 -0400

    fix: add reactive behaviour to Accordion component

--- src/components/Accordion/Accordion.jsx
+++ src/components/Accordion/Accordion.jsx
@@ -1,22 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Accordion = ({ children, title = 'Click to open' }) => {
+  const [open, setOpen] = useState(false);
+
   return (
     <div id="accordion-collapse" data-accordion="collapse">
       <h2 id="accordion-collapse-heading-1">
-        <button type="button" className="r ring-0 focus:ring-0 dark:bg-gray-600 flex items-center justify-between w-full p-5 font-medium rtl:text-right text-gray-500 border border-b-0 border-gray-200 rounded-t-xl focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 gap-3" data-accordion-target="#accordion-collapse-body-1" aria-expanded="true" aria-controls="accordion-collapse-body-1">
+        <button
+          type="button"
+          className="accordion outline-none active:outline-none shadow-none focus:shadow-none dark:bg-gray-600 flex items-center justify-between w-full p-5 font-medium rtl:text-right text-gray-500 border border-b-0 border-gray-200 rounded-t-xl focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 gap-3"
+          aria-expanded={open}
+          aria-controls="accordion-collapse-body-1"
+          onClick={() => setOpen((prev) => !prev)}
+        >
           <span>{title}</span>
-          <svg data-accordion-icon className="w-3 h-3 rotate-180 shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5 5 1 1 5"/>
+          <svg
+            data-accordion-icon
+            className={`w-3 h-3 shrink-0 transition-transform ${open ? 'rotate-180' : ''}`}
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 10 6"
+          >
+            <path
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M9 5 5 1 1 5"
+            />
           </svg>
         </button>
       </h2>
-      <div id="accordion-collapse-body-1" className="hidden" aria-labelledby="accordion-collapse-heading-1">
+      <div
+        id="accordion-collapse-body-1"
+        className={`${open ? '' : 'hidden'}`}
+        aria-labelledby="accordion-collapse-heading-1"
+      >
         <div className="p-5 border border-b-0 border-gray-200 dark:border-gray-700 dark:bg-gray-900">
           {children}
         </div>
       </div>
-     
     </div>
   )
 }
